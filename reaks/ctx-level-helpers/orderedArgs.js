3a1abdec27e7595ce69300eec154fd8c00738d7a
Quentin Vuilliot <quentin.vuilliot@kaes.fr>
2017-09-13 17:22:45 +0200

    Toggle buttons (WIP)

--- reaks/ctx-level-helpers/orderedArgs.js
+++ reaks/ctx-level-helpers/orderedArgs.js
@@ -1,5 +1,17 @@
+const contextualize = require("./contextualize")
 const contextualizeOrderedArgs = require("./contextualizeOrderedArgs")
 
-module.exports = transform => args => ctx => {
-  return transform(contextualizeOrderedArgs(args, ctx))
-}
+module.exports = transform =>
+  function(opts, args) {
+    return ctx => {
+      if (arguments.length === 1) {
+        return transform(contextualizeOrderedArgs(opts, ctx))
+      }
+      if (arguments.length === 2) {
+        return transform(
+          contextualize(opts, ctx),
+          contextualizeOrderedArgs(args, ctx)
+        )
+      }
+    }
+  }
