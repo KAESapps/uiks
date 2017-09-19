70b5cd24697d44ddae2fb41ff218d39cd285b1f6
Sylvain Vuilliot <sylvain.vuilliot@kaes.fr>
2017-09-19 14:48:22 +0200

    expose dialogArgs to simplify augmenting it

--- reaks-material/dialog.js
+++ reaks-material/dialog.js
@@ -1,9 +1,18 @@
+const isFunction = require("lodash/isFunction")
 const contextualize = require("../reaks/ctx-level-helpers/contextualize")
 
-module.exports = ({ title, content, actions, modal }) => ctx => () =>
-  ctx.dialog({
-    title: contextualize(title, ctx),
-    content: contextualize(content, ctx),
-    modal,
-    actions: actions.map(action => contextualize(action, ctx)),
-  })
+const dialogArgs = ({ title, content, actions, modal }) => ctx => ({
+  title: contextualize(title, ctx),
+  content: contextualize(content, ctx),
+  modal,
+  actions: actions.map(action => contextualize(action, ctx)),
+})
+
+const dialog = arg => ctx => () => {
+  const args = isFunction(arg) ? arg(ctx) : dialogArgs(arg)(ctx)
+  return ctx.dialog(args)
+}
+
+dialog.dialogArgs = dialogArgs
+
+module.exports = dialog
