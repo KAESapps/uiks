5511af7d47fceb72b575def016b4868225a27c0e
Quentin Vuilliot <quentin.vuilliot@kaes.fr>
2017-09-21 10:38:33 +0200

    z-pile: content sized (need to choose a layer)

--- reaks-layout/zPile.js
+++ reaks-layout/zPile.js
@@ -1,6 +1,9 @@
+const isArray = require("lodash/isArray")
 const child = require("reaks/child")
 const seq = require("reaks/seq")
 const style = require("reaks/style")
+
+const normalizeLayerArg = layerArg => isArray(layerArg) ? layerArg : [{}, layerArg]
 
 module.exports = function(opts, layers) {
   if (arguments.length === 1) {
@@ -13,20 +16,25 @@ module.exports = function(opts, layers) {
   return seq([
     setPositionRelative && style({ position: "relative" }),
     seq(
-      layers.map((layer, i) =>
-        child(
+      layers.map((layer, i) => {
+        const [opts, cmp] = normalizeLayerArg(layer)
+        return child(
           seq([
-            layer,
-            style({
+            cmp,
+            !opts.sizer ? style({
               position: "absolute",
               top: 0,
               bottom: 0,
               left: 0,
               right: 0,
               zIndex: i,
+            }) : style({
+              position: 'relative',
+              zIndex: i,
             }),
           ])
         )
+      }
       )
     ),
   ])
