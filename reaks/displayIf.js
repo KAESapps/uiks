f9e60d67befdce826e158d5bc69fb6a64ea1e894
Quentin Vuilliot <quentin.vuilliot@kaes.fr>
2017-09-22 10:12:44 +0200

    displayIf with condition from ctx

--- reaks/displayIf.js
+++ reaks/displayIf.js
@@ -1,4 +1,16 @@
+const isString = require("lodash/isString")
 const swap = require("reaks/swap")
 const ctxCmp = require("../reaks/ctx-level-helpers/component")
 
-module.exports = ctxCmp((condition, cmp) => swap(() => condition() && cmp))
+module.exports = ctxCmp(
+  (condition, cmp) => {
+    return swap(() => condition() && cmp)
+  },
+  function(condition, cmp) {
+    if (isString(condition)) {
+      const ctxProp = condition
+      condition = ctx => ctx[ctxProp]
+    }
+    return [condition, cmp]
+  }
+)
