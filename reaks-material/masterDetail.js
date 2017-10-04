e890200f39fa9ffa20bd1a77ee63d10bf9679a3c
Quentin Vuilliot <quentin.vuilliot@kaes.fr>
2017-10-04 12:27:49 +0200

    reaks-material/masterDetail: detail views can access activeItem as value in context

--- reaks-material/masterDetail.js
+++ reaks-material/masterDetail.js
@@ -7,6 +7,7 @@ const align = require("../reaks/align")
 const innerMargin = require("../reaks/innerMargin")
 const iconButton = require("./iconButton")
 const closeIcon = require("./icons/content/clear")
+const value = require("../core/value")
 
 module.exports = ({ master, detail, detailActions }) => {
   return hFlex([
@@ -22,39 +23,42 @@ module.exports = ({ master, detail, detailActions }) => {
               color: "#ddd",
             },
           },
-          vFlex([
-            [
-              "fixed",
-              border(
-                {
-                  b: {
-                    width: 1,
-                    color: "#ddd",
+          value(
+            ctx => ctx.activeItem,
+            vFlex([
+              [
+                "fixed",
+                border(
+                  {
+                    b: {
+                      width: 1,
+                      color: "#ddd",
+                    },
                   },
-                },
-                hFlex([
-                  innerMargin({ h: 16, v: 8 }, hPile(detailActions)),
-                  [
-                    "fixed",
-                    align(
-                      { v: "center" },
-                      innerMargin(
-                        { r: 8 },
-                        iconButton(
-                          ctx => ({
-                            icon: closeIcon,
-                            color: ctx.colors.iconDefault,
-                          }),
-                          ctx => () => ctx.setActiveItem(null)
+                  hFlex([
+                    innerMargin({ h: 16, v: 8 }, hPile(detailActions)),
+                    [
+                      "fixed",
+                      align(
+                        { v: "center" },
+                        innerMargin(
+                          { r: 8 },
+                          iconButton(
+                            ctx => ({
+                              icon: closeIcon,
+                              color: ctx.colors.iconDefault,
+                            }),
+                            ctx => () => ctx.setActiveItem(null)
+                          )
                         )
-                      )
-                    ),
-                  ],
-                ])
-              ),
-            ],
-            detail,
-          ])
+                      ),
+                    ],
+                  ])
+                ),
+              ],
+              detail,
+            ])
+          )
         )
       ),
     ],
