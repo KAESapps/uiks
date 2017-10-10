e5dcc78ed5b88a4b57b1b980885b567354290368
Quentin Vuilliot <quentin.vuilliot@kaes.fr>
2017-10-10 17:28:17 +0200

    responsive side panel

--- reaks-material/sidePanel.js
+++ reaks-material/sidePanel.js
@@ -1,22 +1,43 @@
+const create = require("lodash/create")
 const ctxCmp = require("../reaks/ctx-level-helpers/component")
 const hFlex = require("../reaks/hFlex").reaks
 const size = require("../reaks/size").reaksWrapper
 const border = require("../reaks/border").reaksWrapper
+const { observable } = require("reactivedb/obs")
+const withResponsiveSize = require("../reaks/withResponsiveSize")
 
-module.exports = ctxCmp((panel, content) =>
-  hFlex([
-    [
-      "fixed",
-      border(
+module.exports = ({ panel, content, breakWidth }) =>
+  withResponsiveSize(breakWidth, ctx => {
+    const isOpenNarrow = observable(false)
+    const isOpenLarge = observable(true)
+
+    const newCtx = create(ctx, {
+      togglePanel: () => {
+        if (ctx.responsiveSize() === "narrow") {
+          isOpenNarrow(!isOpenNarrow())
+        } else {
+          isOpenLarge(!isOpenLarge())
+        }
+      },
+      // use this method after a click on menu item in panel for instance
+      // handles auto-close on mobile
+      panelDefaultAction: () => {
+        if (ctx.responsiveSize() === "narrow") {
+          isOpenNarrow(false)
+        }
+      },
+    })
+
+    return hFlex([
+      [
         {
-          r: {
-            width: 1,
-            color: "#ddd",
-          },
+          weight: () =>
+            (ctx.responsiveSize() === "narrow" ? isOpenNarrow() : isOpenLarge())
+              ? null
+              : 0,
         },
-        size({ w: 300 }, panel)
-      ),
-    ],
-    content,
-  ])
-)
+        panel(newCtx),
+      ],
+      content(newCtx),
+    ])
+  })
