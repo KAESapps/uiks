de60580cf85ad7cb06dd23a6e20f4ea5a48b36b4
Quentin Vuilliot <quentin.vuilliot@kaes.fr>
2017-10-16 11:40:10 +0200

    Material-root can take getFileUrl from context

--- reaks-material/root.js
+++ reaks-material/root.js
@@ -4,32 +4,46 @@ const insertCss = require("insert-css")
 const fontFaceRule = require("font-face-rule")
 const wrapper = require("../reaks/ctx-level-helpers/wrapper")
 
-module.exports = wrapper(({ getFileUrl }) =>
-  seq([
-    // add font-face rules
-    () => {
-      const rules = [
-        fontFaceRule("Roboto", {
-          src: [`url('${getFileUrl("Roboto-Regular.woff")}')`],
-          "font-weight": "normal",
-          "font-style": "normal",
-        }),
-        fontFaceRule("Roboto", {
-          src: [`url('${getFileUrl("Roboto-Medium.woff")}')`],
-          "font-weight": "500",
-          "font-style": "normal",
-        }),
-        fontFaceRule("Roboto", {
-          src: [`url('${getFileUrl("Roboto-Bold.woff")}')`],
-          "font-weight": "bold",
-          "font-style": "normal",
-        }),
-      ]
-      const styleElement = insertCss(rules.join("\n"))
-      return () => styleElement.parentElement.removeChild(styleElement)
+module.exports = wrapper(
+  ({ getFileUrl }) =>
+    seq([
+      // add font-face rules
+      () => {
+        const rules = [
+          fontFaceRule("Roboto", {
+            src: [`url('${getFileUrl("Roboto-Regular.woff")}')`],
+            "font-weight": "normal",
+            "font-style": "normal",
+          }),
+          fontFaceRule("Roboto", {
+            src: [`url('${getFileUrl("Roboto-Medium.woff")}')`],
+            "font-weight": "500",
+            "font-style": "normal",
+          }),
+          fontFaceRule("Roboto", {
+            src: [`url('${getFileUrl("Roboto-Bold.woff")}')`],
+            "font-weight": "bold",
+            "font-style": "normal",
+          }),
+        ]
+        const styleElement = insertCss(rules.join("\n"))
+        return () => styleElement.parentElement.removeChild(styleElement)
+      },
+      style({
+        fontFamily: "Roboto",
+      }),
+    ]),
+  {
+    mapArgs: function() {
+      if (arguments.length === 1) {
+        return arguments
+      } else {
+        return [
+          {
+            getFileUrl: ctx => ctx.getFileUrl,
+          },
+        ]
+      }
     },
-    style({
-      fontFamily: "Roboto",
-    }),
-  ])
+  }
 )
