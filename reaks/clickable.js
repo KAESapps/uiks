8653389381c452c26e236f090c66a13d766ff441
Sylvain Vuilliot <sylvain@kaes.fr>
2017-10-19 09:28:29 +0200

    stopPropagation in clickable

--- reaks/clickable.js
+++ reaks/clickable.js
@@ -4,5 +4,11 @@ const onEvent = require("reaks/onEvent")
 const style = require("reaks/style")
 
 module.exports = wrapper(action => {
-  return seq([onEvent("click", action), style({ cursor: "pointer" })])
+  return seq([
+    onEvent("click", ev => {
+      ev.stopPropagation()
+      action(ev)
+    }),
+    style({ cursor: "pointer" }),
+  ])
 })
