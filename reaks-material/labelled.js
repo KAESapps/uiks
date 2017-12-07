dc5b7910421703f76be2185244db5365621f1091
Quentin Vuilliot <quentin.vuilliot@kaes.fr>
2017-12-07 16:39:13 +0100

    labelled + mail icon

---
+++ reaks-material/labelled.js
@@ -0,0 +1,25 @@
+const component = require("../reaks/ctx-level-helpers/component")
+const singleLineText = require("../reaks/label").reaks
+const seq = require("reaks/seq")
+const style = require("reaks/style")
+const vPile = require("../reaks-layout/vPile")
+const colors = require("material-colors")
+const margin = require("../reaks-layout/margin")
+
+module.exports = component(function(label, opts, cmp) {
+  if (arguments.length === 2) {
+    cmp = opts
+    opts = { margin: 0 }
+  }
+  return vPile([
+    seq([
+      singleLineText(label),
+      style({
+        fontSize: 12,
+        color: colors.darkText.secondary,
+      }),
+      margin({ b: opts.margin }),
+    ]),
+    cmp,
+  ])
+})
--- reaks-material/labelledText.js
+++ reaks-material/labelledText.js
@@ -1,24 +1,9 @@
 const component = require("../reaks/ctx-level-helpers/component")
+const labelled = require("./labelled").reaks
 const singleLineText = require("../reaks/label").reaks
-const seq = require("reaks/seq")
-const style = require("reaks/style")
-const vPile = require("../reaks-layout/vPile")
-const colors = require("material-colors")
-const margin = require("../reaks-layout/margin")
 
 module.exports = component(
-  (label, value) =>
-    vPile([
-      seq([
-        singleLineText(label),
-        style({
-          fontSize: 12,
-          color: colors.darkText.secondary,
-        }),
-        margin({ b: 8 }),
-      ]),
-      singleLineText(value),
-    ]),
+  (label, value) => labelled(label, { margin: 8 }, singleLineText(value)),
   function(arg) {
     if (arguments.length === 1) {
       return [arg, ctx => ctx.value]
