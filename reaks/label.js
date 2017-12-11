0096ad23cab1998cafbfb74f4f6c2da7e3b7920c
Quentin Vuilliot <quentin.vuilliot@kaes.fr>
2017-12-11 17:50:32 +0100

    label with empty value of same height as label with value

--- reaks/label.js
+++ reaks/label.js
@@ -1,12 +1,23 @@
+const isFunction = require("lodash/isFunction")
 const component = require("./ctx-level-helpers/component")
 const text = require("reaks/text")
 const style = require("reaks/style")
 const seq = require("reaks/seq")
 
+const nonBreakingWhitespaceChar = "\xa0"
 module.exports = component(
   function(arg) {
+    let textArg
+
+    // keep height even with empty value
+    if (isFunction(arg)) {
+      textArg = () => arg() || nonBreakingWhitespaceChar
+    } else {
+      textArg = arg || nonBreakingWhitespaceChar
+    }
+
     return seq([
-      text(arg),
+      text(textArg),
       style({
         whiteSpace: "nowrap",
         textOverflow: "ellipsis",
