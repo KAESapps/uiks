78f7a534c5e2eca2e5958594c4fa996267c43571
Quentin Vuilliot <quentin.vuilliot@kaes.fr>
2018-01-12 17:10:05 +0100

    fix withSize

--- reaks/withSize.js
+++ reaks/withSize.js
@@ -5,7 +5,7 @@ const sizeDetector = require("./sizeDetector")
 
 module.exports = cmp => ctx => {
   const size = observable(null)
-  let loading = true
+  let destroy
   const reaksCmp = cmp(create(ctx, { size }))
   return seq([
     sizeDetector(node => {
@@ -13,10 +13,14 @@ module.exports = cmp => ctx => {
         height: node.offsetHeight,
         width: node.offsetWidth,
       })
-      if (loading) {
-        reaksCmp(node)
-        loading = false
+      if (!destroy) {
+        // create cmp when size is detected
+        destroy = reaksCmp(node)
       }
     }),
+    () => () => {
+      destroy && destroy()
+      destroy = null
+    },
   ])
 }
