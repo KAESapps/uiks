9cfe64ae212283b8f31e50c2b75be9fc2fee4978
Quentin Vuilliot <quentin.vuilliot@kaes.fr>
2018-01-12 18:02:32 +0100

    Extract flex styles

--- reaks-layout/flex/index.js
+++ reaks-layout/flex/index.js
@@ -4,65 +4,8 @@
 const createDiv = require("reaks/child")
 const seq = require("reaks/seq")
 const concat = require("lodash/concat")
-const isFunction = require("lodash/isFunction")
-const assign = require("lodash/assign")
 const defaults = require("lodash/defaults")
-const style = require("reaks/style")
-
-const flexAlign = a => {
-  let flexAlign = a
-
-  if (a === "top") {
-    flexAlign = "flex-start"
-  }
-  if (a === "bottom") {
-    flexAlign = "flex-end"
-  }
-  if (a === "left") {
-    flexAlign = "flex-start"
-  }
-  if (a === "right") {
-    flexAlign = "flex-end"
-  }
-
-  return flexAlign
-}
-
-const asFlexParent = ({ orientation, wrap, align, overflowAllowed }) =>
-  style(
-    assign(
-      {
-        display: "flex",
-        flexDirection: orientation,
-        flexWrap: wrap ? "wrap" : null,
-        alignItems: flexAlign(align),
-      },
-      !overflowAllowed && {
-        overflow: "hidden",
-      }
-    )
-  )
-
-const staticFlexChildStyle = ({ weight, wrap, align }) => {
-  return assign(
-    weight !== null
-      ? {
-          flex: weight,
-          overflow: wrap ? null : "hidden",
-        }
-      : { display: "flex", flexShrink: 0 },
-    { alignSelf: flexAlign(align) }
-  )
-}
-
-const flexChildStyle = arg => {
-  const { weight } = arg
-  if (isFunction(weight)) {
-    return () => staticFlexChildStyle(assign({}, arg, { weight: weight() }))
-  } else {
-    return staticFlexChildStyle(arg)
-  }
-}
+const { parent: asFlexParent, child: flexChildStyle } = require("./style")
 
 const convertChildOpts = childOpts => {
   if (childOpts == "flex") return { weight: 1 }
@@ -101,10 +44,7 @@ module.exports = ({
         defaultChildOpts
       )
       return createChild(
-        seq([
-          style(flexChildStyle({ orientation, wrap, weight, align })),
-          childMixin,
-        ])
+        seq([flexChildStyle({ orientation, wrap, weight, align }), childMixin])
       )
     })
     return seq(
---
+++ reaks-layout/flex/style.js
@@ -0,0 +1,58 @@
+const isFunction = require("lodash/isFunction")
+const assign = require("lodash/assign")
+const style = require("reaks/style")
+
+const flexAlign = a => {
+  let flexAlign = a
+
+  if (a === "top") {
+    flexAlign = "flex-start"
+  }
+  if (a === "bottom") {
+    flexAlign = "flex-end"
+  }
+  if (a === "left") {
+    flexAlign = "flex-start"
+  }
+  if (a === "right") {
+    flexAlign = "flex-end"
+  }
+
+  return flexAlign
+}
+
+exports.parent = ({ orientation, wrap, align, overflowAllowed }) =>
+  style(
+    assign(
+      {
+        display: "flex",
+        flexDirection: orientation,
+        flexWrap: wrap ? "wrap" : null,
+        alignItems: flexAlign(align),
+      },
+      !overflowAllowed && {
+        overflow: "hidden",
+      }
+    )
+  )
+
+const staticFlexChildStyle = ({ weight, wrap, align }) => {
+  return assign(
+    weight !== null
+      ? {
+          flex: weight,
+          overflow: wrap ? null : "hidden",
+        }
+      : { display: "flex", flexShrink: 0 },
+    { alignSelf: flexAlign(align) }
+  )
+}
+
+exports.child = arg => {
+  const { weight } = arg
+  return style(
+    isFunction(weight)
+      ? () => staticFlexChildStyle(assign({}, arg, { weight: weight() }))
+      : staticFlexChildStyle(arg)
+  )
+}
