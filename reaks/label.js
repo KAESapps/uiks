e7028ab71bfda597fb396153636c911c749b072b
Quentin Vuilliot <quentin.vuilliot@kaes.fr>
2018-01-29 15:32:53 +0100

    Label with optional ellipsis

--- reaks/label.js
+++ reaks/label.js
@@ -6,24 +6,31 @@ const seq = require("reaks/seq")
 
 const nonBreakingWhitespaceChar = "\xa0"
 module.exports = component(
-  function(arg) {
-    let textArg
-
-    // keep height even with empty value
-    if (isFunction(arg)) {
-      textArg = () => arg() || nonBreakingWhitespaceChar
+  function(arg1, arg2) {
+    let labelArg, opt
+    if (arguments.length === 1) {
+      labelArg = arg1
+      opt = { ellipsis: true }
     } else {
-      textArg = arg || nonBreakingWhitespaceChar
+      labelArg = arg2
+      opt = arg1
     }
 
-    return seq([
-      text(textArg),
-      style({
-        whiteSpace: "nowrap",
-        textOverflow: "ellipsis",
-        overflow: "hidden",
-      }),
-    ])
+    let textArg
+    // keep height even with empty value
+    if (isFunction(labelArg)) {
+      textArg = () => labelArg() || nonBreakingWhitespaceChar
+    } else {
+      textArg = labelArg || nonBreakingWhitespaceChar
+    }
+
+    const styleArg = {
+      overflow: "hidden",
+      whiteSpace: "nowrap",
+      textOverflow: opt.ellipsis && "ellipsis",
+    }
+
+    return seq([text(textArg), style(styleArg)])
   },
   // map arguments
   function() {
