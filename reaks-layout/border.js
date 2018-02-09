73ee82a11a80e27800b0cfedb5b50e968cf4e8a3
Quentin Vuilliot <quentin.vuilliot@kaes.fr>
2018-02-09 12:06:37 +0100

    border radius option

--- reaks-layout/border.js
+++ reaks-layout/border.js
@@ -3,9 +3,11 @@ const assign = require("lodash/assign")
 const defaults = require("lodash/defaults")
 const mapValues = require("lodash/mapValues")
 const fromPairs = require("lodash/fromPairs")
+const pick = require("lodash/pick")
 
-const allBorders = partialArg =>
-  fromPairs(["t", "r", "b", "l"].map(s => [s, partialArg]))
+const SIDES = ["t", "r", "b", "l"]
+
+const allBorders = partialArg => fromPairs(SIDES.map(s => [s, partialArg]))
 
 const borderStyle = v => v && `${v.width}px ${v.type} ${v.color}`
 
@@ -27,7 +29,13 @@ module.exports = (arg = defaultPartialArg) => {
   if (arg.all || !(arg.t || arg.r || arg.b || arg.l)) {
     arg = assign(allBorders(arg.all), arg)
   }
-  arg = mapValues(arg, border => defaults({}, border, localDefaultPartialArg))
+  arg = assign(
+    {},
+    arg,
+    mapValues(pick(arg, SIDES), border =>
+      defaults({}, border, localDefaultPartialArg)
+    )
+  )
 
   const { t, r, b, l } = arg
   return style({
@@ -35,5 +43,6 @@ module.exports = (arg = defaultPartialArg) => {
     borderRight: borderStyle(r),
     borderBottom: borderStyle(b),
     borderLeft: borderStyle(l),
+    borderRadius: arg.radius,
   })
 }
