a1308e16ee44b2d06fa357ca619ada6efc5df955
Sylvain Vuilliot <sylvain@kaes.fr>
2018-03-22 15:02:54 +0100

    allow to specify entity in remotePropEditor

--- reactivedb/remotePropEditor.js
+++ reactivedb/remotePropEditor.js
@@ -7,27 +7,39 @@
  */
 
 const create = require("lodash/create")
+const isFunction = require("lodash/isFunction")
 const { observable, transaction } = require("kobs")
 
-module.exports = (prop, view) => ctx => {
+module.exports = (arg, view) => ctx => {
+  const { entity, prop } =
+    typeof arg === "string" ? { entity: "value", prop: arg } : arg
+  const getEntity = isFunction(entity) ? entity(ctx) : entity
+  const getProp = isFunction(prop) ? prop(ctx) : prop
+
   let beforeSubmitTimeout, afterSubmitTimeout
-  const defaultValue = ""
+  const defaultValue = "" // TODO: rendre paramétrable ?
   const submitDelay = 100 // délai avant soumission du input (debounce)
   const displayModeDelay = 1000 // délai avant passage du mode edit au mode display
   const editMode = observable(false)
   const inputValue = observable(defaultValue)
   const submit = () => {
     const newValue = inputValue()
-    ctx.patch({ [ctx.value]: { [prop]: newValue } })
+    ctx.patch({
+      [isFunction(getEntity) ? getEntity() : getEntity]: {
+        [isFunction(getProp) ? getProp() : getProp]: newValue,
+      },
+    })
     clearTimeout(afterSubmitTimeout)
     afterSubmitTimeout = setTimeout(() => editMode(false), displayModeDelay) // on repasse en mode display
   }
 
   const value = observable(() => {
     if (editMode()) return inputValue()
+    const e = isFunction(getEntity) ? getEntity() : getEntity
+    if (!e) return defaultValue
     const remoteValue = ctx.query([
-      { constant: ctx.value },
-      { valueOfProp: prop },
+      { constant: e },
+      { valueOfProp: isFunction(getProp) ? getProp() : getProp },
     ])
     return remoteValue.loaded ? remoteValue.value : defaultValue
   })
