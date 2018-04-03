20089c50508c05a4b7da98f48c3cfc59ad8a37e8
Quentin Vuilliot <quentin.vuilliot@kaes.fr>
2018-04-03 18:02:05 +0200

    textInputLook relooked

--- reaks-material/textInputLook.js
+++ reaks-material/textInputLook.js
@@ -1,9 +1,16 @@
 const seq = require("reaks/seq")
+const size = require("reaks/size")
 const wrapper = require("../reaks/ctx-level-helpers/wrapper")
 const border = require("uiks/reaks-layout/border")
+const align = require("uiks/reaks-layout/align")
 const innerMargin = require("uiks/reaks-layout/innerMargin")
 
 // make a label look like a text input
 module.exports = wrapper(() =>
-  seq([border({ b: { color: "#AAA" } }), innerMargin({ b: 6 })])
+  seq([
+    size({ hMin: 32 }),
+    innerMargin({ h: 3 }),
+    align({ v: "center" }),
+    border({ b: { color: "#AAA" } }),
+  ])
 )
