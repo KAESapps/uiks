38d11e1053b436c528b666701505c063af9a6bf6
Quentin Vuilliot <quentin.vuilliot@kaes.fr>
2018-06-27 14:42:50 +0200

    New section component

--- reaks-layout/zPile.js
+++ reaks-layout/zPile.js
@@ -3,7 +3,8 @@ const child = require("reaks/child")
 const seq = require("reaks/seq")
 const style = require("reaks/style")
 
-const normalizeLayerArg = layerArg => isArray(layerArg) ? layerArg : [{}, layerArg]
+const normalizeLayerArg = layerArg =>
+  isArray(layerArg) ? layerArg : [{}, layerArg]
 
 module.exports = function(opts, layers) {
   if (arguments.length === 1) {
@@ -21,21 +22,23 @@ module.exports = function(opts, layers) {
         return child(
           seq([
             cmp,
-            !opts.sizer ? style({
-              position: "absolute",
-              top: 0,
-              bottom: 0,
-              left: 0,
-              right: 0,
-              zIndex: i,
-            }) : style({
-              position: 'relative',
-              zIndex: i,
-            }),
+            !opts.sizer
+              ? style({
+                  position: "absolute",
+                  top: 0,
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  zIndex: i,
+                })
+              : style({
+                  position: "relative",
+                  zIndex: i,
+                  flex: 1,
+                }),
           ])
         )
-      }
-      )
+      })
     ),
   ])
 }
---
+++ reaks-material/section.js
@@ -0,0 +1,34 @@
+const vFlex = require("uiks/reaks/vFlex")
+const label = require("uiks/reaks/label")
+const innerMargin = require("uiks/reaks/innerMargin")
+const style = require("uiks/reaks/style")
+const align = require("uiks/reaks/align")
+const size = require("uiks/reaks/size")
+const margin = require("uiks/reaks/margin")
+
+module.exports = args => {
+  const { title, content, action } = args
+  return style(
+    {
+      borderRadius: 2,
+      boxShadow:
+        "rgba(0, 0, 0, 0.2) 0px 1px 4px, rgba(0, 0, 0, 0.2) 0px 1px 2px",
+    },
+    vFlex([
+      [
+        "fixed",
+        style(
+          ctx => ({
+            fontWeight: 500,
+            backgroundColor: ctx.colors.primary,
+            height: 36,
+          }),
+          innerMargin({ h: 12 }, align({ v: "center" }, label(title)))
+        ),
+      ],
+      ["fixed", align({ h: "right" }, margin({ t: -24, r: 12 }, action))],
+      ["fixed", size.mixin({ h: 4 })],
+      content,
+    ])
+  )
+}
