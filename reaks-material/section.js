fcaa45dae6370537b4c2867f75a07473ec24f2d9
Quentin Vuilliot <quentin.vuilliot@kaes.fr>
2018-07-04 20:33:02 +0200

    Fix section title color

--- reaks-material/section.js
+++ reaks-material/section.js
@@ -24,6 +24,7 @@ module.exports = args => {
             ctx => ({
               fontWeight: 500,
               backgroundColor: ctx.colors.primary,
+              color: ctx.colors.textOnPrimary,
               height: 36,
             }),
             innerMargin(
