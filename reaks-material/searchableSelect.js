defd86657cbc4e953b605fde26602e13d95e447e
Quentin Vuilliot <quentin.vuilliot@kaes.fr>
2018-09-07 11:29:11 +0200

    searchable select

---
+++ reaks-material/searchableSelect.js
@@ -0,0 +1,52 @@
+const concat = require("lodash/concat")
+const assignObservable = require("uiks/core/assignObservable")
+const observableAsValue = require("uiks/core/observableAsValue")
+const vFlex = require("uiks/reaks/vFlex")
+const hFlex = require("uiks/reaks/hFlex")
+const size = require("uiks/reaks/size")
+const valueLoadingAs = require("uiks/reactivedb/valueLoadingAs")
+const selectableList = require("uiks/reaks/selectableList")
+const dialog = require("uiks/reaks-material/dialog")
+const textInput = require("uiks/reaks-material/textInput")
+const icon = require("uiks/reaks-material/icon")
+const searchIcon = require("uiks/reaks-material/icons/action/search")
+
+module.exports = ({ title, listValue, itemLabel, actions }) =>
+  dialog({
+    title,
+    content: assignObservable(
+      {
+        search: null,
+      },
+      size(
+        { h: 300 },
+        vFlex({ gap: 12 }, [
+          [
+            "fixed",
+            hFlex({ align: "center", gap: 5 }, [
+              [
+                "fixed",
+                icon({
+                  icon: searchIcon,
+                  color: ctx => ctx.colors.fadedDarkText,
+                }),
+              ],
+              observableAsValue(
+                "search",
+                textInput({ placeholder: "Recherche" })
+              ),
+            ]),
+          ],
+          selectableList({
+            value: valueLoadingAs([], listValue),
+            itemLabel,
+            onSelect: ctx => v => {
+              ctx.setValue(v)
+              ctx.closePopup()
+            },
+          }),
+        ])
+      )
+    ),
+    actions: concat(actions, dialog.cancelButton()),
+  })
--- reaks/selectableList.js
+++ reaks/selectableList.js
@@ -1,29 +1,36 @@
-const table = require("./tableWithMargins")
 const value = require("../core/value")
 const clickable = require("../reaks/clickable")
 const border = require("../reaks/border")
 const label = require("../reaks/label")
-const group = require("../reaks/group")
+const align = require("../reaks/align")
 const hoverable = require("../reaks/hoverable")
 const style = require("../reaks/style")
+const innerMargin = require("../reaks/innerMargin")
 const assignCtx = require("../core/assign")
+const vListVirtualScroll = require("uiks/reaks/vListVirtualScroll")
 
 module.exports = args => {
   const cmp = value(
     args.value,
     border(
-      { all: true, b: false },
-      table(
-        {
-          rowMixin: group([
-            hoverable.mixin({
+      vListVirtualScroll({
+        itemHeight: 42,
+        item: border(
+          { b: true },
+          hoverable(
+            {
               over: style.mixin({ backgroundColor: "rgba(0,0,0,0.1)" }),
-            }),
-            clickable.mixin(ctx => () => ctx.setValue(ctx.value)),
-          ]),
-        },
-        [[{ margin: { h: 8 } }, label(args.itemLabel)]]
-      ).body
+            },
+            clickable(
+              ctx => () => ctx.setValue(ctx.value),
+              align(
+                { v: "center" },
+                innerMargin({ h: 8 }, label(args.itemLabel))
+              )
+            )
+          )
+        ),
+      })
     )
   )
   return args.onSelect ? assignCtx({ setValue: args.onSelect }, cmp) : cmp
