0fbfd785a1826944c1669f36756971f313a5573c
Sylvain Vuilliot <sylvain.vuilliot@kaes.fr>
2018-09-18 10:43:29 +0200

    default getter for mapValue

--- core/mapValue.js
+++ core/mapValue.js
@@ -1,6 +1,10 @@
 const isFunction = require("lodash/isFunction")
 
 module.exports = (mapFn, getValue) => ctx => {
-  const ctxValue = getValue(ctx)
-  return isFunction(ctxValue) ? () => mapFn(ctxValue()) : mapFn(ctxValue)
+  const ctxValue = getValue ? getValue(ctx) : ctx.value
+  return isFunction(ctxValue)
+    ? () => {
+        return mapFn(ctxValue())
+      }
+    : mapFn(ctxValue)
 }
