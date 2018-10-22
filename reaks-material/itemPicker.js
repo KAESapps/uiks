77fb29a0b5df1d82b18b7823f9755c3851f38718
Sylvain Vuilliot <sylvain.vuilliot@kaes.fr>
2018-10-22 19:18:07 +0200

    more customizable itemPicker

--- reaks-material/itemPicker.js
+++ reaks-material/itemPicker.js
@@ -1,12 +1,13 @@
+const isFunction = require("lodash/isFunction")
 const selectDialog = require("./select")
 const selectButton = require("./selectButton")
 
-module.exports = ({ items, label }) =>
+module.exports = ({ items, label, title }) =>
   selectButton(
-    ctx => () => label(ctx.value()),
+    ctx => () => label(isFunction(ctx.value) ? ctx.value() : ctx.value, ctx),
     selectDialog({
-      title: "Choisir un élément",
+      title: title || "Choisir un élément",
       value: Array.isArray(items) ? () => () => items : items,
-      itemLabel: ctx => () => label(ctx.value),
+      itemLabel: ctx => () => label(ctx.value, ctx),
     })
   )
