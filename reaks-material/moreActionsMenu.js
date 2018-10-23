5fa37fc488f06b149d173a3da02e32779b7b955c
Quentin Vuilliot <quentin.vuilliot@kaes.fr>
2018-10-23 16:42:09 +0200

    moreActionMenu with displayIf option

--- reaks-material/moreActionsMenu.js
+++ reaks-material/moreActionsMenu.js
@@ -1,21 +1,31 @@
+const isPlainObject = require("lodash/isPlainObject")
+const displayIf = require("../reaks/displayIf")
 const vPile = require("../reaks/vPile")
 const menuIcon = require("./icons/navigation/moreVertical")
 const iconButton = require("./iconButton")
 const flatButton = require("./flatButton")
 const dialog = require("./dialog")
 
+const optionalDisplayIf = (cond, view) => (cond ? displayIf(cond, view) : view)
+
 module.exports = actions => rootCtx =>
   iconButton(
     { icon: menuIcon, color: ctx => ctx.colors.textOnPrimary },
     dialog({
       content: vPile(
-        actions.map(([text, action]) =>
-          flatButton(text, ctx => () => {
-            // TODO: attendre le retour d'un promise pour fermer le menu ?
-            ctx.closePopup()
-            action(rootCtx)()
-          })
-        )
+        actions.map(([opts, action]) => {
+          const { label, displayIf: cond } = isPlainObject(opts)
+            ? opts
+            : { label: opts }
+          return optionalDisplayIf(
+            cond,
+            flatButton(label, ctx => () => {
+              // TODO: attendre le retour d'un promise pour fermer le menu ?
+              ctx.closePopup()
+              action(rootCtx)()
+            })
+          )
+        })
       ),
       actions: [dialog.cancelButton()],
     })
