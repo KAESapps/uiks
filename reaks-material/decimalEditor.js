cce606c1c1ec5f0ae627d6bcbbfb3d075346d5e0
Quentin Vuilliot <quentin.vuilliot@kaes.fr>
2018-11-13 13:48:51 +0100

    decimalEditor refactor

--- reaks-material/decimalEditor.js
+++ reaks-material/decimalEditor.js
@@ -1,16 +1,11 @@
-const formatDecimal = require("reactivedb/operators/formatDecimal")
 const mapValue = require("uiks/core/mapValue")
 const decimalPad = require("./decimalPad")
 const textInputLook = require("./textInputLook")
 const label = require("uiks/reaks/label")
+const formatDecimalFromInt = require("reactivedb/operators/formatDecimalFromInt")
 
-const decimalDisplayer = ({ decimals }) => {
-  const toFloat = int => int / Math.pow(10, decimals)
-  const formatFloat = formatDecimal(decimals)
-  const format = v => formatFloat(toFloat(v))
-
-  return textInputLook(label(mapValue(format)))
-}
+const decimalDisplayer = ({ decimals }) =>
+  textInputLook(label(mapValue(formatDecimalFromInt(decimals))))
 
 const dialogEditor = require("./dialogEditor")
 
