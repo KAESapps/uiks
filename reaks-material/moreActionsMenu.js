085e6136374bf04826c4c8d7ff0c978fe6f0ac7f
Quentin Vuilliot <quentin.vuilliot@kaes.fr>
2018-11-28 16:47:38 +0100

    moreActionsMenu options

--- reaks-material/moreActionsMenu.js
+++ reaks-material/moreActionsMenu.js
@@ -1,9 +1,14 @@
+const assign = require("lodash/assign")
 const menuIcon = require("./icons/navigation/moreVertical")
 const iconButton = require("./iconButton")
 const actionsPopup = require("./actionsPopup")
 
-module.exports = actions =>
-  iconButton(
-    { icon: menuIcon, color: ctx => ctx.colors.textOnPrimary },
+module.exports = function(opts, actions) {
+  if (arguments.length === 1) {
+    actions = opts
+  }
+  return iconButton(
+    assign({ icon: menuIcon, color: ctx => ctx.colors.textOnPrimary }, opts),
     actionsPopup(actions)
   )
+}
