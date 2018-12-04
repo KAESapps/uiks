a6b903be52966c073c38a328651df39b5c68b1d6
Quentin Vuilliot <quentin.vuilliot@kaes.fr>
2018-12-04 16:18:43 +0100

    switchBoolean : empty by default

--- reaks/switchBoolean.js
+++ reaks/switchBoolean.js
@@ -1,6 +1,8 @@
-const swap = require('reaks/swap')
-const isFunction = require('lodash/isFunction')
+const swap = require("reaks/swap")
+const isFunction = require("lodash/isFunction")
+const empty = require("./empty")
+
 module.exports = (condition, cases) => ctx => {
   const cond = isFunction(condition) ? condition(ctx) : condition
-  return swap(() => (cond() ? cases['truthy'] : cases['falsy'])(ctx))
+  return swap(() => (cases[cond() ? "truthy" : "falsy"] || empty)(ctx))
 }
