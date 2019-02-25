d7d007ab9d1c2dae26cc6c29b2e4bbc6e5e9e149
Quentin Vuilliot <quentin.vuilliot@kaes.fr>
2019-02-25 14:38:45 +0100

    switches with static condtions

--- reaks/loadingSwitch.js
+++ reaks/loadingSwitch.js
@@ -1,11 +1,14 @@
 const create = require("lodash/create")
 const swap = require("reaks/swap")
 
-module.exports = ({ loaded, loading }) => ctx => {
-  return swap(
-    () =>
-      ctx.value().loaded
-        ? loaded(create(ctx, { value: () => ctx.value().value }))
-        : loading && loading(ctx)
+module.exports = ({ loaded, loading, staticValue = false }) => ctx => {
+  return swap(() =>
+    ctx.value().loaded
+      ? loaded(
+          create(ctx, {
+            value: staticValue ? ctx.value().value : () => ctx.value().value,
+          })
+        )
+      : loading && loading(ctx)
   )
 }
--- reaks/switchBoolean.js
+++ reaks/switchBoolean.js
@@ -2,7 +2,12 @@ const swap = require("reaks/swap")
 const isFunction = require("lodash/isFunction")
 const empty = require("./empty")
 
-module.exports = (condition, cases) => ctx => {
-  const cond = isFunction(condition) ? condition(ctx) : condition
-  return swap(() => (cases[cond() ? "truthy" : "falsy"] || empty)(ctx))
+module.exports = (arg1, arg2) => ctx => {
+  const conditionArg = arg2 ? arg1 : ctx.value
+  const cases = arg2 ? arg2 : arg1
+
+  const cond = isFunction(conditionArg) ? conditionArg(ctx) : conditionArg
+  return isFunction(cond)
+    ? swap(() => (cases[cond() ? "truthy" : "falsy"] || empty)(ctx))
+    : (cases[cond ? "truthy" : "falsy"] || empty)(ctx)
 }
