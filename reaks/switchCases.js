04536699f502f4f6218198d85125c7783bbdeb6f
Quentin Vuilliot <quentin.vuilliot@kaes.fr>
2019-04-03 07:44:09 +0200

    switchCases accepts static conditions

--- reaks/switchCases.js
+++ reaks/switchCases.js
@@ -1,10 +1,13 @@
+const isFunction = require("lodash/isFunction")
 const swap = require("reaks/swap")
 const empty = require("./empty")
 
 module.exports = (condition, cases, def = empty) => ctx => {
   const cond = condition(ctx)
-  return swap(() => {
-    const c = cond()
-    return (c in cases ? cases[c] : def)(ctx)
-  })
+  return isFunction(cond)
+    ? swap(() => {
+        const c = cond()
+        return (c in cases ? cases[c] : def)(ctx)
+      })
+    : (cond in cases ? cases[cond] : def)(ctx)
 }
