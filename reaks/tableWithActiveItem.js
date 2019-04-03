4ac99d74cdf1e9f9c1f30cfc28efb8f8c07a1123
Quentin Vuilliot <quentin.vuilliot@kaes.fr>
2019-04-03 07:44:33 +0200

    Extracted rowMixin from tableWithActiveItem

--- reaks/tableWithActiveItem.js
+++ reaks/tableWithActiveItem.js
@@ -3,28 +3,34 @@ const clickable = require("./clickable")
 const activeStyle = require("./activeStyle")
 const activeItem = require("../core/activeItem")
 
-module.exports = (arg1, arg2) => {
+// si pas de next, on utilise la nouvelle convention "openItem", sinon la convention "setActiveItem/ensureItemVisible/next"
+const rowMixin = (opts = {}) =>
+  activeStyle(
+    clickable.mixin(
+      opts.next
+        ? activeItem(ctx => () => ctx.value, opts.next)
+        : ctx =>
+            ctx.openItem
+              ? () => ctx.openItem(ctx.value)
+              : () => {
+                  ctx.setActiveItem(ctx.value)
+                  ctx.ensureItemVisible && ctx.ensureItemVisible(true)
+                }
+    )
+  )
+
+const tableGenerator = (arg1, arg2) => {
   const opts = arg2 ? arg1 : {}
   const columns = arg2 ? arg2 : arg1
 
-  // si pas de next, on utilise la nouvelle convention "openItem", sinon la convention "setActiveItem/ensureItemVisible/next"
   return table(
     {
-      rowMixin: activeStyle(
-        clickable.mixin(
-          opts.next
-            ? activeItem(ctx => () => ctx.value, opts.next)
-            : ctx =>
-                ctx.openItem
-                  ? () => ctx.openItem(ctx.value)
-                  : () => {
-                      ctx.setActiveItem(ctx.value)
-                      ctx.ensureItemVisible && ctx.ensureItemVisible(true)
-                    }
-        )
-      ),
+      rowMixin: rowMixin(opts),
       itemHeight: opts.itemHeight,
     },
     columns
   )
 }
+tableGenerator.rowMixin = rowMixin
+
+module.exports = tableGenerator
