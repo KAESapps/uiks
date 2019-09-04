82bb45ee863e6bad9a571c3b9e84064ba89313b8
Quentin Vuilliot <quentin.vuilliot@kaes.fr>
2019-09-04 12:29:49 +0200

    new icons

---
+++ reaks-material/icons/content/add.js
@@ -0,0 +1,4 @@
+module.exports = {
+  path: "M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z",
+  viewBox: "0 0 24 24",
+}
---
+++ reaks-material/icons/content/remove.js
@@ -0,0 +1,4 @@
+module.exports = {
+  path: "M19 13H5v-2h14v2z",
+  viewBox: "0 0 24 24",
+}
