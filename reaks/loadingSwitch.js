c610eaa65cf1e8f3acebdb9befb719ed4439c677
Quentin Vuilliot <quentin.vuilliot@kaes.fr>
2019-09-06 12:09:43 +0200

    Fix loadingSwitch: was unnecessarily reloading component when in 'loaded' state

--- reaks/loadingSwitch.js
+++ reaks/loadingSwitch.js
@@ -2,13 +2,16 @@ const create = require("lodash/create")
 const swap = require("reaks/swap")
 
 module.exports = ({ loaded, loading, staticValue = false }) => ctx => {
+  const dynamicCmp = loaded(create(ctx, { value: () => ctx.value().value }))
   return swap(() =>
     ctx.value().loaded
-      ? loaded(
-          create(ctx, {
-            value: staticValue ? ctx.value().value : () => ctx.value().value,
-          })
-        )
+      ? staticValue
+        ? loaded(
+            create(ctx, {
+              value: ctx.value().value,
+            })
+          )
+        : dynamicCmp
       : loading && loading(ctx)
   )
 }
