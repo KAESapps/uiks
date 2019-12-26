65a97fa6e66d1fbc225f867c64bb36880960abbc
Quentin Vuilliot <quentin.vuilliot@kaes.fr>
2019-12-26 08:52:57 +0100

    textInputLook option : backgroundColor

--- reaks-material/textInputLook.js
+++ reaks-material/textInputLook.js
@@ -4,14 +4,16 @@ const wrapper = require("../reaks/ctx-level-helpers/wrapper")
 const border = require("uiks/reaks-layout/border")
 const align = require("uiks/reaks-layout/align")
 const innerMargin = require("uiks/reaks-layout/innerMargin")
+const style = require("reaks/style")
 
 // make a label look like a text input
 module.exports = wrapper(
-  ({ border: withBorder = true, borderColor = "#AAA" } = {}) =>
+  ({ border: withBorder = true, borderColor = "#AAA", backgroundColor } = {}) =>
     seq([
       size({ hMin: 32 }),
       innerMargin({ h: 6 }),
       align({ v: "center" }),
       withBorder && border({ color: borderColor, radius: 3 }),
+      style({ backgroundColor }),
     ])
 )
