4609733a27c7e4a82eca1b440a5bf9f533222397
Sylvain Vuilliot <sylvain.vuilliot@kaes.fr>
2020-01-21 13:42:10 +0100

    page fix moreActions

--- reaks-material/page.js
+++ reaks-material/page.js
@@ -1,17 +1,19 @@
+const concat = require("lodash/concat")
 const isArray = require("lodash/isArray")
 const contextualize = require("../reaks/ctx-level-helpers/contextualize")
 const actionLayout = require("./pageActionLayout")
 const moreActionsMenu = require("./moreActionsMenu")
 
-module.exports = function(args) {
-  const actions = isArray(args.action)
-    ? args.action
-    : args.action
-    ? [args.action]
-    : []
-  if (args.moreActions && args.moreActions.length) {
-    actions.push(moreActionsMenu(args.moreActions))
-  }
+module.exports = function (args) {
+  // let actions = isArray(args.action)
+  //   ? args.action
+  //   : args.action
+  //     ? [args.action]
+  //     : []
+  // if (args.moreActions && args.moreActions.length) {
+  //   actions = concat(actions, moreActionsMenu(args.moreActions))
+  // }
+  const actions = concat(args.action, (args.moreActions && args.moreActions.length) ? moreActionsMenu(args.moreActions) : [])
 
   return ctx => {
     return {
