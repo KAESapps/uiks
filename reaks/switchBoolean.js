98874dfa3ebc2778f4916743b0afba6e85720f7a
Quentin Vuilliot <quentin.vuilliot@kaes.fr>
2020-03-02 15:36:58 +0100

    reaks level switch cases/boolean

--- reaks/switchBoolean.js
+++ reaks/switchBoolean.js
@@ -1,8 +1,15 @@
 const swap = require("reaks/swap")
 const isFunction = require("lodash/isFunction")
+const reaksEmpty = require("lodash/noop")
 const empty = require("./empty")
 
-module.exports = (arg1, arg2) => ctx => {
+const reaksSwitch = (condition, cases) => {
+  return isFunction(condition)
+    ? swap(() => cases[condition() ? "truthy" : "falsy"] || reaksEmpty)
+    : cases[condition ? "truthy" : "falsy"] || reaksEmpty
+}
+
+const uiksSwitch = (arg1, arg2) => ctx => {
   const conditionArg = arg2 ? arg1 : ctx.value
   const cases = arg2 ? arg2 : arg1
 
@@ -11,3 +18,7 @@ module.exports = (arg1, arg2) => ctx => {
     ? swap(() => (cases[cond() ? "truthy" : "falsy"] || empty)(ctx))
     : (cases[cond ? "truthy" : "falsy"] || empty)(ctx)
 }
+
+uiksSwitch.reaks = reaksSwitch
+
+module.exports = uiksSwitch
--- reaks/switchCases.js
+++ reaks/switchCases.js
@@ -1,8 +1,21 @@
 const isFunction = require("lodash/isFunction")
+const reaksEmpty = require("lodash/noop")
+const mapValues = require("lodash/mapValues")
 const swap = require("reaks/swap")
 const empty = require("./empty")
 
-module.exports = (condition, cases, def = empty) => ctx => {
+const reaksSwitchCases = (condition, cases, def = reaksEmpty) => {
+  return isFunction(condition)
+    ? swap(() => {
+        const c = condition()
+        return c in cases ? cases[c] : def
+      })
+    : condition in cases
+    ? cases[condition]
+    : def
+}
+
+const uiksSwitchCases = (condition, cases, def = empty) => ctx => {
   const cond = condition(ctx)
   cases = isFunction(cases) ? cases(ctx) : cases
   return isFunction(cond)
@@ -12,3 +25,7 @@ module.exports = (condition, cases, def = empty) => ctx => {
       })
     : (cond in cases ? cases[cond] : def)(ctx)
 }
+
+uiksSwitchCases.reaks = reaksSwitchCases
+
+module.exports = uiksSwitchCases
