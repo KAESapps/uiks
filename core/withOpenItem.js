4c53d1a109a251bbf83ad8a4a678394d2bf27e7b
Quentin Vuilliot <quentin.vuilliot@kaes.fr>
2020-03-02 15:37:18 +0100

    withOpenItem with inherit option

--- core/withOpenItem.js
+++ core/withOpenItem.js
@@ -1,10 +1,11 @@
+const identity = require("lodash/identity")
 const withActiveItem = require("./withActiveItem")
 const activeItem = require("./activeItem")
 const withEnsureItemVisible = require("./withEnsureItemVisible")
 const ctxAssign = require("./assign")
 
-module.exports = ({ next }, cmp) =>
-  withActiveItem(
+module.exports = ({ next, inheritActiveItem }, cmp) =>
+  (inheritActiveItem ? identity : withActiveItem)(
     withEnsureItemVisible(
       ctxAssign({ openItem: activeItem(ctx => id => id, next) }, cmp)
     )
