f8d306bd80024d20a465d8855fc1437b3b79aa4b
Sylvain Vuilliot <sylvain.vuilliot@kaes.fr>
2020-06-26 15:40:45 +0200

    numberEditor: remove "?" for non numeric values

--- reaks-material/numberEditor.js
+++ reaks-material/numberEditor.js
@@ -2,13 +2,13 @@ const get = require("lodash/get")
 const numericPad = require("./numericPad")
 const textInputLook = require("./textInputLook")
 const label = require("uiks/reaks/label")
-const numberToString = n =>
-  get(n, "toLocaleString") ? n.toLocaleString("fr") : "?"
+const numberToString = (n) =>
+  get(n, "toLocaleString") ? n.toLocaleString("fr") : ""
 
 const textFieldDisplayer = textInputLook(
-  label(ctx => () => numberToString(ctx.value()))
+  label((ctx) => () => numberToString(ctx.value()))
 )
 
 const dialogEditor = require("./dialogEditor")
 
-module.exports = opt => dialogEditor(numericPad(opt), textFieldDisplayer)
+module.exports = (opt) => dialogEditor(numericPad(opt), textFieldDisplayer)
