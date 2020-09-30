11bb8bb6ef845b0c54482779947024447fcdaec0
Quentin Vuilliot <quentin.vuilliot@kaes.fr>
2020-09-30 12:29:35 +0200

    loadingSwitch with static option does not preload component

--- reaks/loadingSwitch.js
+++ reaks/loadingSwitch.js
@@ -2,7 +2,8 @@ const create = require("lodash/create")
 const swap = require("reaks/swap")
 
 module.exports = ({ loaded, loading, staticValue = false }) => ctx => {
-  const dynamicCmp = loaded(create(ctx, { value: () => ctx.value().value }))
+  const dynamicCmp =
+    !staticValue && loaded(create(ctx, { value: () => ctx.value().value }))
   return swap(() =>
     ctx.value().loaded
       ? staticValue
