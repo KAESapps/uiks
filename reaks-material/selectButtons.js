b10b3f6355d93d4e79c7ce39f55ca78786c61b89
Quentin Vuilliot <quentin.vuilliot@kaes.fr>
2022-11-22 17:20:34 +0100

    select buttons with icons

--- reaks-material/selectButtons.js
+++ reaks-material/selectButtons.js
@@ -1,5 +1,6 @@
 const orderedArgsCmp = require("../reaks/ctx-level-helpers/orderedArgs")
 const hFlex = require("../reaks/hFlex").reaks
+const vPile = require("../reaks/vPile").reaks
 const isString = require("lodash/isString")
 const clickable = require("../reaks/clickable").reaksMixin
 const label = require("../reaks/label").reaks
@@ -7,16 +8,31 @@ const style = require("../reaks/style")
 const size = require("../reaks/size")
 const align = require("../reaks/align").reaks
 const innerMargin = require("../reaks/innerMargin").reaksMixin
+const icon = require("./icon").reaks
 const seq = require("reaks/seq")
 const defaults = require("lodash/defaults")
 
 const toggleButton = (
   text,
+  iconDef,
   onAction,
   isActive,
   { activeTextColor, defaultTextColor, activeBackgroundColor }
-) =>
-  seq([
+) => {
+  const iconCmp =
+    iconDef &&
+    icon({
+      icon: iconDef,
+      color: () => (isActive() ? activeTextColor : defaultTextColor),
+    })
+  const textCmp =
+    text && style.reaksWrapper(iconDef ? { fontSize: 12 } : {}, label(text))
+  const content =
+    iconCmp && textCmp
+      ? vPile({ align: "center" }, [iconCmp, textCmp])
+      : iconCmp || textCmp
+
+  return seq([
     clickable(onAction),
     innerMargin({ h: 16 }),
     align({ h: "center", v: "center" }),
@@ -30,11 +46,12 @@ const toggleButton = (
             color: defaultTextColor,
           }
     }),
-    label(text),
+    content,
   ])
+}
 
 module.exports = orderedArgsCmp(
-  function(opts, args) {
+  function (opts, args) {
     if (arguments.length === 1) {
       args = opts
       opts = {}
@@ -55,21 +72,22 @@ module.exports = orderedArgsCmp(
           "rgba(0, 0, 0, 0.2) 0px 1px 4px, rgba(0, 0, 0, 0.2) 0px 1px 2px",
       },
       size.reaksWrapper(
-        { h: 36 },
+        { hMin: 36 },
         hFlex(
-          args.map(([{ isActive, onAction }, label]) =>
-            toggleButton(label, onAction, isActive, {
+          args.map(([{ isActive, onAction }, arg]) => {
+            const { text, icon: iconDef } = isString(arg) ? { text: arg } : arg
+            return toggleButton(text, iconDef, onAction, isActive, {
               activeTextColor,
               defaultTextColor,
               activeBackgroundColor,
             })
-          )
+          })
         )
       )
     )
   },
   {
-    itemArg: function(opts, items) {
+    itemArg: function (opts, items) {
       if (arguments.length === 1) {
         items = opts
         opts = {}
