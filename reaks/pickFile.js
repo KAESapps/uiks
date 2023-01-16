d6e07d0cc76feab84b081a5d723b4313ea0a7410
Sylvain Vuilliot <sylvain.vuilliot@kaes.fr>
2023-01-16 17:02:40 +0100

    pickFile cancel

--- reaks/pickFile.js
+++ reaks/pickFile.js
@@ -1,26 +1,31 @@
 module.exports = ({ accept, multi } = {}) => {
-  const fileInput = document.createElement("input")
-  fileInput.type = "file"
-  fileInput.accept = accept
-  fileInput.style.visibility = "hidden"
-  if (multi) fileInput.multiple = true
-
-  document.body.appendChild(fileInput)
-  fileInput.focus()
-  fileInput.click()
-
   return new Promise((resolve, reject) => {
-    function checkFiles() {
-      document.body.removeChild(this)
+    const fileInput = document.createElement("input")
+    fileInput.type = "file"
+    fileInput.accept = accept
+    if (multi) fileInput.multiple = true
+    fileInput.style.display = "none"
+    document.body.appendChild(fileInput)
 
-      if (this.files.length) {
-        resolve(multi ? Array.from(this.files) : this.files[0])
-      } else {
-        reject(this.files)
-      }
+    //exécuté seulement si l'utilisateur sélectionne un fichier... mais après le onWindowFocus
+    const onFilesChange = () => {
+      resolve(multi ? Array.from(fileInput.files) : fileInput.files[0])
     }
 
-    fileInput.addEventListener("focus", checkFiles, { once: true })
-    fileInput.addEventListener("change", checkFiles, { once: true })
+    //éxécuté dans tous les cas (avant l'événement onFilesChange si l'utilisateur a sélectionné un fichier)
+    //c'est pourquoi on met un timeout de 400ms pour faire un reject finalement si il n'y a pas eu de resolve avant
+    const onWindowFocus = ev => {
+      window.removeEventListener("focus", onWindowFocus)
+      document.body.removeChild(fileInput)
+      setTimeout(() => {
+        fileInput.removeEventListener("change", onFilesChange)
+        reject("canceled") // ne sert à rien s'il y a déjà eu un resolve avant
+      }, 400)
+    }
+
+    fileInput.focus()
+    fileInput.click()
+    fileInput.addEventListener("change", onFilesChange)
+    window.addEventListener("focus", onWindowFocus)
   })
 }
