f0fcf79b23bfc73b3a9beff94324ce6a1e1fb620
Quentin Vuilliot <quentin.vuilliot@kaes.fr>
2023-06-08 16:08:57 +0200

    textInput flex

--- reaks-material/textInput.js
+++ reaks-material/textInput.js
@@ -1,6 +1,5 @@
 const child = require("reaks/child")
 const style = require("reaks/style")
-const size = require("reaks/size")
 const seq = require("reaks/seq")
 const ctxCmp = require("../reaks/ctx-level-helpers/component")
 const textInputLook = require("./textInputLook")
@@ -17,35 +16,40 @@ module.exports = ctxCmp(
     password = false,
     autoFocus = false,
   }) =>
-    child(
-      seq([
-        style({
-          border: "none",
-          outline: "none",
-          font: "inherit",
-          backgroundColor: "transparent",
-        }),
-        size({ w: "100%" }),
-        textInputLook.reaksMixin(),
-        attrs({
-          placeholder: placeholder,
-          type: password ? "password" : null,
-        }),
-        valueAttr(value), // specialized attr handler that prevent cursor jumping
-        onEvent("input", ev => {
-          setValue(ev.target.value || null)
-        }),
-        onEvent("click", ev => {
-          ev.stopPropagation() // prevent unwanted effects of propagation of the click event to container
-        }),
-        autoFocus &&
-          (domNode => {
-            domNode.focus()
-            process.env.PLATFORM === "windows" && domNode.click()
+    seq([
+      style({
+        display: "flex",
+      }),
+      child(
+        seq([
+          style({
+            flex: 1,
+            border: "none",
+            outline: "none",
+            font: "inherit",
+            backgroundColor: "transparent",
           }),
-      ]),
-      () => document.createElement("input")
-    ),
+          textInputLook.reaksMixin(),
+          attrs({
+            placeholder: placeholder,
+            type: password ? "password" : null,
+          }),
+          valueAttr(value), // specialized attr handler that prevent cursor jumping
+          onEvent("input", ev => {
+            setValue(ev.target.value || null)
+          }),
+          onEvent("click", ev => {
+            ev.stopPropagation() // prevent unwanted effects of propagation of the click event to container
+          }),
+          autoFocus &&
+            (domNode => {
+              domNode.focus()
+              process.env.PLATFORM === "windows" && domNode.click()
+            }),
+        ]),
+        () => document.createElement("input")
+      ),
+    ]),
   // defaults args from context
   function (arg) {
     return [
