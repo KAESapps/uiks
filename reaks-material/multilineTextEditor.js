0b317d28a8fe54cb25e16b449a8706e706b3fb8c
Sylvain Vuilliot <sylvain.vuilliot@kaes.fr>
2023-10-04 16:11:16 +0200

    readOnly ediors

--- reaks-material/multilineTextEditor.js
+++ reaks-material/multilineTextEditor.js
@@ -5,16 +5,25 @@ const innerMargin = require("../reaks/innerMargin")
 const multilineText = require("../reaks/multilineText")
 const dialogEditor = require("./dialogEditor")
 const scroll = require("uiks/reaks/scroll")
+const switchBoolean = require("../reaks/switchBoolean")
 
 const multilineTextFieldDisplayer = size(
   { h: 100, wMin: 256 },
   border({ radius: 2 }, scroll(innerMargin(8, multilineText())))
 )
 
-module.exports = dialogEditor(
-  multilineTextInput({
-    autoFocus: true,
-  }),
-  multilineTextFieldDisplayer,
-  { title: ctx => ctx.model.label }
+module.exports = switchBoolean(
+  ctx => {
+    return ctx.readOnly
+  },
+  {
+    truthy: multilineTextFieldDisplayer,
+    falsy: dialogEditor(
+      multilineTextInput({
+        autoFocus: true,
+      }),
+      multilineTextFieldDisplayer,
+      { title: ctx => ctx.model.label }
+    ),
+  }
 )
