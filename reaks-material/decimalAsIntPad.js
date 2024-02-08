16368b2caa653096bb5e0ef021b6d1c37d8844bc
Sylvain Vuilliot <sylvain.vuilliot@kaes.fr>
2024-02-08 14:18:43 +0100

    numericPad avec un mode replace à l'init

--- reaks-material/decimalAsIntPad.js
+++ reaks-material/decimalAsIntPad.js
@@ -1,11 +1,14 @@
-const toInteger = require("lodash/toInteger")
-const padEnd = require("lodash/padEnd")
-const toString = require("lodash/toString")
-const padStart = require("lodash/padStart")
+const round = require("lodash/round")
 const numericPad = require("./numericPad")
+const assignCtx = require("uiks/core/assign")
+
+//2024-02-08 : changement de l'implémentation pour passer du mode entier à décimal, avant c'était de la manipulation de texte maintenant c'est juste de la mulitplication/division
+//l'idée de la manipulation de texte était d'éviter les problèmes de représentation en JS des nombres décimaux mais avec la fonction round, ça doit être bon
+// l'intérêt de l'approche mathématique est qu'on n'affiche que les décimales significatives dans l'éditeur (2,1 au lieu de 2,100) et que l'implémentation est plus simple
 
 module.exports = ({ decimalOffset, decimals, allowNegative }) => {
-  if (decimalOffset != null) decimals = decimalOffset //TODO: tenir compte de "decimals" aussi dans ce cas
+  if (!decimals) decimals = 0
+  if (decimalOffset == null) decimalOffset = decimals
   const validateValue = decimalString => {
     const commaPosition = decimalString.indexOf(",")
     return (
@@ -13,38 +16,22 @@ module.exports = ({ decimalOffset, decimals, allowNegative }) => {
       decimalString.length - 1 - commaPosition <= decimals
     )
   }
-  return numericPad({
-    decimal: !!decimals,
-    allowNegative,
-    toNumber: false,
-    validateValue,
-    fromExternalValue: integer => {
-      if (integer == null) return ""
-
-      const sign = integer < 0 ? "-" : ""
-      const integerString = padStart(toString(Math.abs(integer)), decimals, "0")
-      if (!decimals) return integerString
-
-      const intPart = integerString.slice(0, -decimals)
-      const decPart = integerString.slice(-decimals)
-      return sign + (intPart || "0") + "," + decPart
+  return assignCtx(
+    {
+      value: ctx => () => {
+        const val = ctx.value()
+        return val == null ? null : val / Math.pow(10, decimalOffset)
+      },
+      setValue: ctx => val =>
+        ctx.setValue(
+          val == null ? null : round(val * Math.pow(10, decimalOffset))
+        ),
     },
-    toExternalValue: decimalString => {
-      if (decimalString === "") return null
-      if (!decimals) return toInteger(decimalString)
-
-      let sign = 1
-      if (decimalString[0] === "-") {
-        sign = -1
-        decimalString = decimalString.slice(1)
-      }
-
-      const [intPart, decPart] = decimalString.split(",")
-      return (
-        sign *
-        (toInteger(intPart) * Math.pow(10, decimals) +
-          toInteger(padEnd(decPart, decimals, "0")))
-      )
-    },
-  })
+    numericPad({
+      decimal: !!decimals,
+      allowNegative,
+      toNumber: true,
+      validateValue,
+    })
+  )
 }
